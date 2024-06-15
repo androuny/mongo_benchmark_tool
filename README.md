1b1fa988c960ffca1292ba505aac654398bdfc99
Androuny <cetri.brali@gmail.com>
2024-06-15 16:05:16 +0300

    updated readme and added interactive setup for the benchmark tool

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 how to run
 `go mod tidy`
+
 `go run .`
--- main.go
+++ main.go
@@ -7,17 +7,35 @@ import (
 )
 
 func main() {
+    var uri string
+    var db_name string
+    var collection_name string
+    var insert_users int
 
-    fmt.Println("Starting MongoDB Benchmark tool...")
+    fmt.Println("Starting MongoDB Benchmark tool... ")
+    fmt.Println("")
 
-    handler, connTime, err := handlers.NewMongoHandler("mongodb://localhost:27017/", "dev_db", "users")
+    fmt.Println("Please enter your mongodb connection uri: ")
+    fmt.Scanf("%s \n", &uri)
+
+    fmt.Println("Please enter your mongodb db name: ")
+    fmt.Scanf("%s \n", &db_name)
+
+    fmt.Println("Please enter your mongodb db collection name: ")
+    fmt.Scanf("%s \n", &collection_name)
+
+    fmt.Println("Please enter the amount of users you want to insert for the test: ")
+    fmt.Scanf("%d \n", &insert_users)
+
+
+    handler, connTime, err := handlers.NewMongoHandler(uri, db_name, collection_name)
     //handler, connTime, err := handlers.NewMongoHandler("mongodb://192.168.0.195/", "dev_db", "users")
     if err != nil {
         log.Fatalf("[DB_INIT] %v ", err)
     }
     fmt.Printf("Database Connection Success, took %v ms \n", connTime.Milliseconds())
 
-    genTime, writeTime, err2 := handler.MakeNewUsers(10_000)
+    genTime, writeTime, err2 := handler.MakeNewUsers(insert_users)
     if err2 != nil {
         log.Fatalf("[DB_WRITE] %v", err2)
     }
